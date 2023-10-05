26e13b08d1ce8893e774e94b93783c43fb057c50
mohamed ramadan tammam <elmohandesmohamed33@gmail.com>
2023-10-05 21:30:10 +0300

    home_work1(ex2)

---
+++ src/home_work1(ex2).c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+
+ Name        : home_work1(ex2).c
+ Author      : mohamed
+ Version     :
+ Copyright   : home work 1 (ex2)
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x;
+	printf("Enter a interger:");
+	fflush(stdout);
+	scanf("%d",&x);
+	printf("you entered: %d",x);
+	return 0;
+}
